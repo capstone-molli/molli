b50156b1aa9976341ebc1c640a18b16613f7db28
Alan Yoho <alanyoh@gmail.com>
2018-08-09 13:01:42 -0400

    working on redux...

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { AuthenticateAccountView, AccountSetupView, AllStreamView, SingleStreamView } from "./src/components"
 import { createStackNavigator } from 'react-navigation';
+import React, { Component } from "react"
+import store from "./src/db/store"
+import { Provider, connect } from 'react-redux';
 
-const App = createStackNavigator({
-  // AuthenticateAccountView: { screen: AuthenticateAccountView },
-  // AccountSetupView: { screen: AccountSetupView },
+const Stack = createStackNavigator({
+  AuthenticateAccountView: { screen: AuthenticateAccountView },
+  AccountSetupView: { screen: AccountSetupView },
   AllStreamView: { screen: AllStreamView },
   SingleStreamView: { screen: SingleStreamView },
 });
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Stack />
+      </Provider>
+    )
+  }
+}
+
+
