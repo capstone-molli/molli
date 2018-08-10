a5aa6d59b41f16202f4d1971facd8b143e277cba
Alan Yoho <alanyoh@gmail.com>
2018-08-10 14:17:07 -0400

    bug fix

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import { AuthenticateAccountView, AccountSetupView, AllStreamView, SingleStreamView, UserSetupForm } from "./src/components"
+import { AuthenticateAccountView, AccountSetupView, AllStreamView, SingleStreamView } from "./src/components"
 import { createStackNavigator } from 'react-navigation';
 import React, { Component } from "react"
-import store from "./src/db/store"
-import { Provider, connect } from 'react-redux';
 
 const App = createStackNavigator({
   AuthenticateAccountView: { screen: AuthenticateAccountView },
   AccountSetupView: { screen: AccountSetupView },
   AllStreamView: { screen: AllStreamView },
   SingleStreamView: { screen: SingleStreamView },
-  UserSetupForm: { screen: UserSetupForm }
 });
 
 export default App
--- src/db/firebaseMethods.js
+++ src/db/firebaseMethods.js
@@ -1,29 +1,18 @@
 import firestore from "./firebase"
 
 function getUser(id) {
-    firestore.collection("users").doc(id).get().then(user => user.data())
+    return firestore.collection("users").doc(id).get().then(user => user.data())
 }
 
 function updateUser(id, obj) {
     return firestore.collection("users").doc(`${id}`).set({ obj })
 }
 
-
 function createNewUser(id, obj) {
     return firestore.collection("users").doc(`${id}`).set({ obj })
 }
 
 export { createNewUser, getUser, updateUser }
-//create a firebase method that creates a new user object using 
-//id
-// first_name
-// last_name
-// middle_name
-// name
-// name_format
-// picture
-// short_name
-
 
 //Create User
 //Name, Username, Email, Venmo, authToken
