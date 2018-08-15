f2a6717ba6328e522ab920ed7f6166d6e014d559
Alan Yoho <alanyoh@gmail.com>
2018-08-15 19:13:37 -0400

    fixed up a bit

--- src/components/ActiveBets.js
+++ src/components/ActiveBets.js
@@ -4,11 +4,11 @@ import { Card, Button, Icon, Header, Divider } from 'react-native-elements'
 import styles from './styles'
 import axios from 'axios'
 import { twitchData } from "../db/twitchData"
-import {getAllBetsbyUser} from '../db/firebaseMethods'
+import { getAllBetsbyUser } from '../db/firebaseMethods'
 import * as firebase from "firebase"
 
 export default class ActiveBets extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
             user: null,
@@ -18,10 +18,10 @@ export default class ActiveBets extends Component {
         this.expandProfileCard = this.expandProfileCard.bind(this)
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         const user = firebase.auth().currentUser.uid
-        console.log('user : ', user );
-        this.setState({ 
+        console.log('user : ', user);
+        this.setState({
             user
         })
         console.log(this.state.user)
@@ -37,69 +37,69 @@ export default class ActiveBets extends Component {
     }
 
 
-    render(){
-        console.log('bets',this.state.bets)
-        return this.state.loading ? (<View style={{ flex: 1, flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-        <Image style={{ width: 300, height: 300 }} source={require("../assets/loading.gif")} />
-    </View>) : (
-        <View style={{ flex: 1 }}>
-            <View style={{ flex: 2 / 20, backgroundColor: "#228B22" }}>
-                <View style={{ flexDirection: "row", flex: 1 }}>
-                    <View style={{ flex: 2 / 10, flexDirection: "column", justifyContent: "flex-end", alignItems: "center" }}>
-                        <TouchableOpacity onPress={this.expandProfileCard}>
-                            <Image style={{ width: 30, height: 30, bottom: 20 }} source={require("../assets/settings.png")} />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ flex: 8 / 10 }}>
-                        <View>
+    render() {
+        console.log('bets', this.state.bets)
+        return this.state.loading ? (<View style={{ flex: 1, flexDirection: "column", alignItems: "center", justifyContent: "center", backgroundColor: "#FFF" }}>
+            <Image style={{ width: 300, height: 300 }} source={require("../assets/loading-1.gif")} />
+        </View>) : (
+                <View style={{ flex: 1 }}>
+                    <View style={{ flex: 2 / 20, backgroundColor: "#FFF" }}>
+                        <View style={{ flexDirection: "row", flex: 1 }}>
+                            <View style={{ flex: 2 / 10, flexDirection: "column", justifyContent: "flex-end", alignItems: "center" }}>
+                                <TouchableOpacity onPress={this.expandProfileCard}>
+                                    <Image style={{ width: 30, height: 30, bottom: 20 }} source={require("../assets/settings.png")} />
+                                </TouchableOpacity>
+                            </View>
+                            <View style={{ flex: 8 / 10 }}>
+                                <View>
+                                </View>
+                            </View>
                         </View>
                     </View>
-                </View>
-            </View>
-            <View style={{ flex: 18 / 20 }}>
-                    <ScrollView>
-                        
-                        <View > <Text style={styles.titleText} onPress={this.onPressTitle}>
+                    <View style={{ flex: 18 / 20 }}>
+                        <ScrollView>
+
+                            <View > <Text style={styles.titleText} onPress={this.onPressTitle}>
                                 Open Bets With A taker
                                 </Text>
-                            {this.state.bets.openBetsWithTaker.length !== 0  ? this.state.bets.openBetsWithTaker.map(bet => {
-                                return (
+                                {this.state.bets.openBetsWithTaker.length !== 0 ? this.state.bets.openBetsWithTaker.map(bet => {
+                                    return (
                                         <Card
-                                            title={'User Bet Was Placed On: '+ bet.obj.epicUser}>
-                                            <Text style={{marginBottom: 10}}>
+                                            title={'User Bet Was Placed On: ' + bet.obj.epicUser}>
+                                            <Text style={{ marginBottom: 10 }}>
                                                 {'Bet Type: '}{bet.obj.betType}
                                             </Text>
-                                            <Text style={{marginBottom: 10}}>
+                                            <Text style={{ marginBottom: 10 }}>
                                                 {'Bet Amount: $'}{bet.obj.betAmount}
                                             </Text>
                                         </Card>
-                                     )
+                                    )
 
-                            }) : <View><Text>No open bets with takers right now</Text></View>}
-                        </View>
-                        <Divider style={{ backgroundColor: 'grey' }} />
-                        <Divider style={{ backgroundColor: 'grey' }} />
-                        <Divider style={{ backgroundColor: 'grey' }} />
-                        <View > <Text style={styles.titleText} onPress={this.onPressTitle}>
+                                }) : <View><Text>No open bets with takers right now</Text></View>}
+                            </View>
+                            <Divider style={{ backgroundColor: 'grey' }} />
+                            <Divider style={{ backgroundColor: 'grey' }} />
+                            <Divider style={{ backgroundColor: 'grey' }} />
+                            <View > <Text style={styles.titleText} onPress={this.onPressTitle}>
                                 Open Bets Without A taker
                                 </Text>
-                            {this.state.bets.openBetsNoTaker.length !== 0 ? this.state.bets.openBetsNoTaker.map(bet => {
-                                return (
-                                    <Card key = {bet.obj.epicUser}
-                                    title={'User Bet Was Placed On: '+ bet.obj.epicUser}>
-                                        <Text style={{marginBottom: 10}}>
-                                            {'Bet Type: '}{bet.obj.betType}
-                                        </Text>
-                                        <Text style={{marginBottom: 10}}>
-                                            {'Bet Amount: $'}{bet.obj.betAmount}
-                                        </Text>
-                                    </Card>
-                                     )
+                                {this.state.bets.openBetsNoTaker.length !== 0 ? this.state.bets.openBetsNoTaker.map(bet => {
+                                    return (
+                                        <Card key={bet.obj.epicUser}
+                                            title={'User Bet Was Placed On: ' + bet.obj.epicUser}>
+                                            <Text style={{ marginBottom: 10 }}>
+                                                {'Bet Type: '}{bet.obj.betType}
+                                            </Text>
+                                            <Text style={{ marginBottom: 10 }}>
+                                                {'Bet Amount: $'}{bet.obj.betAmount}
+                                            </Text>
+                                        </Card>
+                                    )
 
-                            }) : <Text>No open bets with no takers right now</Text>}
-                        </View>
-                    </ScrollView>
-                </View>
-        </View>)
+                                }) : <Text>No open bets with no takers right now</Text>}
+                            </View>
+                        </ScrollView>
+                    </View>
+                </View>)
     }
 }
--- src/components/SingleStreamView.js
+++ src/components/SingleStreamView.js
@@ -3,7 +3,6 @@ import { View, Image, TouchableOpacity, WebView } from 'react-native';
 import Swiper from "./subComponents/SwiperComponent"
 import SlideUpPanel from "./subComponents/SlideUpPanel"
 import resolveAssetSource from 'resolveAssetSource';
-import { getBet, getAllBets } from '../db/firebaseMethods'
 import Carousel from "./subComponents/Carousel"
 import Cards from "./subComponents/Cards"
 
@@ -18,6 +17,7 @@ export default class SingleStreamView extends Component {
         this.state = {
             visible: false,
             allowDragging: true,
+            loading: true
         }
         this.toggleView = this.toggleView.bind(this)
     }
@@ -32,47 +32,41 @@ export default class SingleStreamView extends Component {
             source: [this.state.visible ? resolveAssetSource(require("../assets/plus.png")) : resolveAssetSource(require("../assets/cancel.png"))]
         })
     }
-
-    // if(bets.length){
-    //     for (let i = 0; i < bets.length; i++){
-    //         arr.push(bets[i].obj)
-    //     }
-    //     console.log(bets, 'this is line 58')
-    //     return arr
-    // }
-
-
+    componentDidMount() {
+        setTimeout(() => this.setState({ loading: false }), 1000);
+    }
     render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <View style={{ flex: 5 / 12, backgroundColor: "#228B22" }}>
-                    <View style={{ flex: 2 / 10, flexDirection: "row", justifyContent: "flex-start", alignItems: "flex-end" }}>
-                        <TouchableOpacity onPress={this.goBack}>
-                            <Image style={{ width: 30, height: 30, bottom: 20 }} source={require("../assets/back.png")} />
-                        </TouchableOpacity>
+        return this.state.loading ? (<View style={{ flex: 1, flexDirection: "column", alignItems: "center", justifyContent: "center", backgroundColor: "#FFF" }}>
+            <Image style={{ width: 300, height: 300 }} source={require("../assets/loading-1.gif")} />
+        </View>) : (
+                <View style={{ flex: 1 }}>
+                    <View style={{ flex: 5 / 12, backgroundColor: "#FFF" }}>
+                        <View style={{ flex: 2 / 10, flexDirection: "row", justifyContent: "flex-start", alignItems: "center" }}>
+                            <TouchableOpacity onPress={this.goBack}>
+                                <Image style={{ width: 40, height: 40 }} source={require("../assets/back.png")} />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{ flex: 8 / 10 }}>
+                            <WebView
+                                allowsInlineMediaPlayback={true}
+                                mediaPlaybackRequiresUserAction={false}
+                                bounces={false}
+                                source={{ uri: `http://player.twitch.tv/?channel=${this.props.navigation.state.params.login}` }}
+                                style={{ marginTop: 0 }}
+                            />
+                        </View>
                     </View>
-                    <View style={{ flex: 8 / 10 }}>
-                        <WebView
-                            allowsInlineMediaPlayback={true}
-                            mediaPlaybackRequiresUserAction={false}
-                            bounces={false}
-                            source={{ uri: `http://player.twitch.tv/?channel=${this.props.navigation.state.params.login}` }}
-                            style={{ marginTop: 0 }}
-                        />
-
+                    <View style={{ flex: 7 / 12 }}>
+                        <Carousel />
+                        <SlideUpPanel visible={this.state.visible} allowDragging={this.state.allowDragging} props={this.props.navigation.state.params} toggleView={this.toggleView} />
+                        <View style={{ flexDirection: "row", justifyContent: "flex-end", right: 15, bottom: 15, position: "absolute" }}>
+                            <TouchableOpacity onPress={this.toggleView}>
+                                <Image ref="plusButton" style={{ width: 80, height: 80 }} source={require('../assets/plus.png')} />
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
-                <View style={{ flex: 7 / 12 }}>
-                    <Carousel />
-                    <SlideUpPanel visible={this.state.visible} allowDragging={this.state.allowDragging} props={this.props.navigation.state.params} toggleView={this.toggleView} />
-                    <View style={{ flexDirection: "row", justifyContent: "flex-end", right: 15, bottom: 15, position: "absolute" }}>
-                        <TouchableOpacity onPress={this.toggleView}>
-                            <Image ref="plusButton" style={{ width: 80, height: 80 }} source={require('../assets/plus.png')} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        )
+            )
 
     }
 }
