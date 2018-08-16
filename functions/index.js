a37cce2ec9c77aa415312ac700c0c77e97dca3b0
Kenneth Lai <knthslai@gmail.com>
2018-08-16 14:06:05 -0400

    package.json fix

--- functions/index.js
+++ functions/index.js
@@ -8,136 +8,216 @@ admin.initializeApp(functions.config().firebase);
 var db = admin.firestore();
 db.settings({ timestampsInSnapshots: true })
 
+exports.betUpdate = functions.firestore.document(`bets/{betId}`).onCreate((snap, context) => {
+  console.log('context', context);
+  const newBet = snap.data().obj;
+  console.log('newBet', newBet);
+
+});
 
 exports.conFetFortniteAPI = functions.https.onRequest((req, res) => {
+  let player = req.query.player
+  db.collection("players").doc(player).get().then(playerInfo => {
 
-  request({
-    method: "GET",
-    url: `https://api.fortnitetracker.com/v1/profile/pc/${req.query.player}`,
-    headers: {
-      "TRN-Api-Key": functions.config().fortniteapi.key
-    },
-    json: true
-  }).then(result => {
-    // console.log('result', result);
-    /*"accountId",
-    "platformId",
-    "platformName",
-    "platformNameLong",
-    "epicUserHandle",
-    "stats",
-    "lifeTimeStats",
-    "recentMatches",*/
-    // db.collection("players").doc(result.epicUserHandle).set(result)
-    const bet = {
+    const playerStatus = playerInfo.data()
 
-      betAmount: "12",
-      betType: "Win",
-      description: "win",
-      epicUser: "DominicSmorraJr",
-      timePlaced: `2018-08-14T16:32:30.19`
-    }
-    // console.log('bet', bet);
-    const match = result.recentMatches[0]
-    // console.log('checking match: ', match);
-    db.collection("matches").doc(result.epicUserHandle).set(match)
+    if (!playerInfo.exists || playerStatus.status !== 'watching' || req.query.status === 'continue') {
+      request({
+        method: "GET",
+        url: `https://api.fortnitetracker.com/v1/profile/pc/${player}`,
+        headers: {
+          "TRN-Api-Key": functions.config().fortniteapi.key
+        },
+        json: true
+      }).then(result => {
 
-    let matchDate = new Date(match.dateCollected)
-    const betDate = new Date(`2018-08-14T17:17:30.19`)
-    matchDate -= (4 * 60 * 60 * 1000)
-    // console.log('match.dateCollected', match.dateCollected);
-    // console.log('match date', d.getDate(), '/', d.getMonth(), '-', [d.d.getHours(), d.getMinutes(), d.getSeconds()].join(':'));
-    console.log('betDate < d', betDate < matchDate);
-    console.log('matchDate', matchDate);
-    console.log('betDate', betDate);
-    if (betDate < matchDate) {
-      if (match.top1 && bet.betType === 'Win') {
-        console.log('Player wins the bet!')
-      } else {
-        console.log('Player lost the bet!')
-      }
-      res.status(201).send()
+        // console.log('result', result);
+        /*"accountId",
+        "platformId",
+        "platformName",
+        "platformNameLong",
+        "epicUserHandle",
+        "stats",
+        "lifeTimeStats",
+        "recentMatches",*/
+        // lifeTimeStats ->7: Matches Played , 8: Wins
+        let oldMatchCount;
+        let oldWins;
+
+        const matchCount = result.lifeTimeStats.filter(ele => ele.key === "Matches Played")[0].value
+        // console.log('matchCount', matchCount);
+        const wins = result.lifeTimeStats.filter(ele => ele.key === "Wins")[0].value
+        // console.log('wins', wins);
+        const bet = {
+
+          betAmount: "12",
+          betType: "Win",
+          description: "win",
+          epicUser: "DominicSmorraJr",
+          timePlaced: `2018-08-14T16:32:30.19`
+        }
+
+        if (playerInfo.exists) {
+          oldMatchCount = playerStatus.lifeTimeStats.filter(ele => ele.key === "Matches Played")[0].value
+          oldWins = playerStatus.lifeTimeStats.filter(ele => ele.key === "Wins")[0].value
+          if (!req.query.status) {
+            console.log(`Watching ${result.epicUserHandle}`)
+            db.collection("players").doc(result.epicUserHandle).update(result)
+            db.collection("players").doc(result.epicUserHandle).update({ status: `watching` })
+          }
+        } else {
+          console.log(`Created and watching ${result.epicUserHandle}`)
+          db.collection("players").doc(result.epicUserHandle).set(result).then(() => {
+            db.collection("players").doc(result.epicUserHandle).update({ status: `watching` })
+          })
+          // db.collection("players").doc(result.epicUserHandle).set(match)
+
+          oldMatchCount = matchCount
+          oldWins = wins
+        }
+
+        let betDate;
+        if (req.query.time) {
+          betDate = new Date(req.query.time)
+        } else {
+          betDate = new Date()
+          // console.log('Setting Bet Date', betDate);
+        }
+        const nowDate = new Date()
+        if (nowDate - betDate >= 20 * 60 * 1000) {
+          console.log("Bet Expired")
+          db.collection("players").doc(result.epicUserHandle).update({ status: `expired` })
+          res.status(200).end()
+        } else {
+
+          if (matchCount > oldMatchCount) {
+            let newResult;
+            if (wins > oldWins && bet.betType === 'Win') {
+              console.log(`Result for ${player}: Player wins the bet!`)
+              newResult = 'win'
+            } else {
+              console.log(`Result for ${player}: Player lost the bet!`)
+              newResult = 'lost'
+            }
+            db.collection("players").doc(result.epicUserHandle).get().then(() => {
+              db.collection("players").doc(result.epicUserHandle).update(result)
+              db.collection("players").doc(result.epicUserHandle).update({ status: `finished`, lastResult: newResult, timeframe: { betDate, nowJS: new Date() } }).then(() => res.status(204).send())
+            })
+
+          } else {
+            // console.log(`No result for ${player}, lets try again`)
+            setTimeout(() => {
+              res.status(200).end()
+              request({
+                method: "GET",
+                url: `https://us-central1-molli-e1c3f.cloudfunctions.net/conFetFortniteAPI?player=${player}&time=${betDate}&status=continue`,
+              })
+            }, 15000)
+
+          }
+        }
+      }).catch(e => console.error(e))
     } else {
-      console.log("No result yet, lets try again")
-      setTimeout(() => {
-        request({
-          method: "GET",
-          url: `https://us-central1-molli-e1c3f.cloudfunctions.net/conFetFortniteAPI?player=${req.query.player}`,
-        })
-        res.status(200).send()
-      }, 20000)
+      console.log(`Already watching`)
+      res.send(`Already watching`)
     }
   }).catch(e => console.error(e))
 })
-// exports.constFetchFortniteAPI = functions.https.onRequest((req, res) => {
-//   const beta = functions.config().betafortniteapi
-//   let fortniteAPI = new Fortnite(
-//     [
-//       beta.user,
-//       beta.password,
-//       beta.clienttoken,
-//       beta.gametoken
-//     ],
-//     {
-//       debug: true
-//     }
-//   );
+exports.betaConFetFortniteAPI = functions.https.onRequest((req, res) => {
+  const beta = functions.config().betafortniteapi
+  let fortniteAPI = new Fortnite(
+    [
+      beta.user,
+      beta.password,
+      beta.clienttoken,
+      beta.gametoken
+    ]
+  )
+  let player = req.query.player
+  db.collection("players2").doc(player).get().then(playerInfo => {
 
-//   fortniteAPI.login().then(() => {
-//     let oldStats;
-//     // console.log(`Watching ${req.querys.player}`)
-//     db.collection("playerStats").doc(req.query.player).get().then(dbStats => {
+    const playerStatus = playerInfo.data()
 
-//       if (!dbStats.exists) {
-//         console.log(`Did not find${req.query.player}`)
-//         fortniteAPI.getStatsBR(req.query.player, "pc", "alltime")
-//           .then(({ group, info, lifetimeStats }) => {
-//             oldStats = { group, info, lifetimeStats, timeUpdated: Date.now() }
-//             db.collection("playerStats").doc(req.query.player).set(oldStats)
-//           })
-//       } else {
-//         oldStats = dbStats.data()
-//         // console.log('Found`${req.query.player}', oldStats);
-//         // console.log('How many minutes ago: ', Math.round(((Date.now() - oldStats.timeUpdated % 86400000) % 3600000) / 60000))
-//         if (Math.round(((Date.now() - oldStats.timeUpdated % 86400000) % 3600000) / 60000) > 15) {
-//           // console.log("Updating`${req.query.player} Record")
-//           fortniteAPI.getStatsBR(`${req.query.player}`, "pc", "alltime")
-//             .then(({ group, info, lifetimeStats }) => {
-//               oldStats = { group, info, lifetimeStats, timeUpdated: Date.now() }
-//               db.collection("playerStats").doc(`${req.query.player}`).set(oldStats)
-//             })
-//         }
-//       }
-//       // console.log('oldStats to compare', oldStats);
-//       fortniteAPI.getStatsBR(req.query.player, "pc", "alltime")
-//         .then(newStats => {
-//           console.log(`Checking ${req.query.player}'s stats`)
-//           if (newStats.lifetimeStats.matches !== oldStats.lifetimeStats.matches) {
-//             // console.log("Results are in!")
-//             if (newStats.lifetimeStats.wins > oldStats.lifetimeStats.wins) {
-//               console.log(`${req.query.player} Won!`)
-//             } else {
-//               console.log(`${req.query.player} Lost!`)
-//             }
-//             db.collection("playerStats").doc(req.query.player).update(newStats)
-//             db.collection("playerStats").doc(req.query.player).update({ timeUpdated: Date.now() })
-//             res.status(201).end()
-//           } else {
-//             db.collection("playerStats").doc(req.query.player).update({ timeUpdated: Date.now() }).then(() => {
-//               setTimeout(() => {
-//                 request({
-//                   method: "GET",
-//                   url: `https://us-central1-molli-e1c3f.cloudfunctions.net/constFetchFortniteAPI?player=${req.query.player}`,
-//                 })
-//                 res.status(201).end()
-//               }, 20000)
-//             })
-//           }
-//         }).catch(err => {
-//           console.log('Keys: ', Object.keys(err))
-//           console.error(err)
-//         })
+    if (!playerInfo.exists || playerStatus.status !== 'watching' || req.query.status === 'continue') {
+      fortniteAPI.login().then(() => {
+        fortniteAPI.getStatsBR(player, "pc", "alltime")
+          .then(result => {
+            // db.collection("players2").doc(player).set(result)
+            let oldMatchCount;
+            let oldWins;
+            player = result.info.username
+            const matchCount = result.lifetimeStats.matches
+            // console.log('matchCount', matchCount);
+            const wins = result.lifetimeStats.wins
+            const bet = {
 
-//     }).catch(err => console.error(err));
-//   }).catch(err => console.error(err))
-// })
+              betAmount: "12",
+              betType: "Win",
+              description: "win",
+              epicUser: "DominicSmorraJr",
+              timePlaced: `2018-08-14T16:32:30.19`
+            }
+
+            if (playerInfo.exists) {
+              oldMatchCount = playerStatus.lifetimeStats.matches
+              oldWins = playerStatus.lifetimeStats.wins
+              if (!req.query.status) {
+                console.log(`Watching ${player}`)
+                db.collection("players2").doc(player).update(result)
+                db.collection("players2").doc(player).update({ status: `watching` })
+              }
+            } else {
+              console.log(`Created and watching ${player}`)
+              db.collection("players2").doc(player).set(result).then(() => {
+                db.collection("players2").doc(player).update({ status: `watching` })
+              })
+              oldMatchCount = matchCount
+              oldWins = wins
+            }
+
+            let betDate;
+            if (req.query.time) {
+              betDate = new Date(req.query.time)
+            } else {
+              betDate = new Date()
+            }
+            const nowDate = new Date()
+            if (nowDate - betDate >= 20 * 60 * 1000) {
+              console.log("Bet Expired")
+              db.collection("players2").doc(player).update({ status: `expired` })
+              res.status(200).end()
+            } else {
+
+              if (matchCount > oldMatchCount) {
+                let newResult;
+                if (wins > oldWins && bet.betType === 'Win') {
+                  console.log(`Result for ${player}: Player wins the bet!`)
+                  newResult = 'win'
+                } else {
+                  console.log(`Result for ${player}: Player lost the bet!`)
+                  newResult = 'lost'
+                }
+                db.collection("players2").doc(player).get().then(() => {
+                  db.collection("players2").doc(player).update(result)
+                  db.collection("players2").doc(player).update({ status: `finished`, lastResult: newResult, timeframe: { betDate, nowJS: new Date() } }).then(() => res.status(204).send())
+                })
+
+              } else {
+                // console.log(`No result for ${player}, lets try again`)
+                setTimeout(() => {
+                  res.status(200).end()
+                  request({
+                    method: "GET",
+                    url: `https://us-central1-molli-e1c3f.cloudfunctions.net/betaConFetFortniteAPI?player=${player}&time=${betDate}&status=continue`,
+                  })
+                }, 8000)
+              }
+            }
+          }).catch(e => console.error(e))
+      }).catch(e => console.error(e))
+    } else {
+      console.log(`Already watching`)
+      res.send(`Already watching`)
+    }
+  }).catch(e => console.error(e))
+})
---
+++ src/server/fortniteAPI.js
@@ -0,0 +1,13 @@
+const Fortnite = require("fortnite-api");
+
+export default new Fortnite(
+  [
+    "knthslai@gmail.com",
+    "mollipasswordAPI1",
+    "MzRhMDJjZjhmNDQxNGUyOWIxNTkyMTg3NmRhMzZmOWE6ZGFhZmJjY2M3Mzc3NDUwMzlkZmZlNTNkOTRmYzc2Y2Y=",
+    "ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ="
+  ],
+  {
+    debug: true
+  }
+);
